1e7f1fc54471c7dce38688b40f87b64496456be2
mitsalgh <121527453+mitsalgh@users.noreply.github.com>
2023-05-22 09:20:11 +0700

    Add files via upload

---
+++ Testing_Board.ino
@@ -0,0 +1,39 @@
+#define rpwm1 3
+#define rpwm2 5
+#define rpwm3 7
+#define rpwm4 9
+#define rpwm5 11
+
+#define lpwm1 4
+#define lpwm2 6
+#define lpwm3 8
+#define lpwm4 10
+#define lpwm5 12
+
+void setup() {
+ pinMode(rpwm1, OUTPUT);
+ pinMode(rpwm2, OUTPUT);
+ pinMode(rpwm3, OUTPUT);
+ pinMode(rpwm4, OUTPUT);
+ pinMode(rpwm5, OUTPUT);
+ 
+ pinMode(lpwm1, OUTPUT);
+ pinMode(lpwm2, OUTPUT);
+ pinMode(lpwm3, OUTPUT);
+ pinMode(lpwm4, OUTPUT);
+ pinMode(lpwm5, OUTPUT);
+
+}
+
+void loop() 
+{
+   analogWrite(rpwm1, 0);
+   analogWrite(lpwm1, 100);
+   Serial.println("buka");
+   delay(750);
+   analogWrite(rpwm1, 150);
+   analogWrite(lpwm1, 0);
+   Serial.println("tutup");
+   delay(550); 
+
+}
