b0d7d0c036e241c79d63366cbd314ad5444cc0cf
mitsalgh <121527453+mitsalgh@users.noreply.github.com>
2023-08-14 08:42:16 +0700

    Update Demo_Vending.ino

--- Demo_Vending.ino
+++ Demo_Vending.ino
@@ -1,133 +1,134 @@
-#include <string.h>
+//#include <string.h>
 #include <SoftwareSerial.h>
 #include <SPI.h>
-#include <Adafruit_PN532.h>
+//#include <Adafruit_PN532.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-//Setting Miso Mosi NFC ID Sensor
-#define SCK  (13)
-#define MOSI (11)
-#define SS   (10)
-#define MISO (12)
-Adafruit_PN532 nfc(SCK, MISO, MOSI, SS);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int relayBawah[5]= {3,5,7,9,11}; // setup pin untuk Relay yang akan disetting di bawah
 
-#define pinButton A1
-#define pwm1 5
-#define pwm2 6
-
-int lcdColumns = 18; 
-int lcdRows = 4;
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
-
-//Variabel
-String nfcID="";
-
-void prosesBuka()
+String dataIn;
+String dataBukaShaf[10];
+int i;
+boolean parsing=false;
+ 
+void setup() 
 {
-   analogWrite(pwm1, 0);
-   analogWrite(pwm2, 200);
-   delay(700);
-   analogWrite(pwm1, 0);
-   analogWrite(pwm2, 0);
-   delay(1000);
-   analogWrite(pwm1, 200);
-   analogWrite(pwm2, 0);
-   delay(500);
-}
-void stopVending()
-{
-  analogWrite(pwm1, 0);
-  analogWrite(pwm2, 0);
-}
-
-void getNfcIdUser()//check NFC ID
-{
-  uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  String CardID = "";
-  String Link = "";
-  if (success) 
-  {
-    unsigned int hex_num;
-    hex_num =  uid[3] << 24;
-    hex_num += uid[2] << 16;
-    hex_num += uid[1] <<  8;
-    hex_num += uid[0];
-    CardID = String(hex_num);
-    if (CardID.length()==8) 
-    {
-      CardID = '0'+CardID;
-    } 
-    if(CardID.length()==9) //panjang karakter untuk nfc id
-    {
-      CardID = '0'+CardID;
-    }
-    else 
-    {
-      nfcID=CardID;
-    }
-    nfcID=CardID;
-    lcd.clear();
-    lcd.setCursor(2, 1);
-    lcd.print("Sedang Diproses");
-    prosesBuka();
-    lcd.clear();
-    lcd.setCursor(3, 1);
-    lcd.print("Silahkan Ambil");
-    delay(2000);
-    lcd.clear();
-  }
-  else
-  {
-    lcd.setCursor(3, 1);
-    lcd.print("TAP KARTU ANDA");
-    stopVending();
-  }
-}
-
-void setup()
-{
+  Serial.begin(9600);
+  dataIn=""; 
   lcd.init();// initialize LCD
   lcd.backlight();
-  lcd.setCursor(0, 0);
-  pinMode(pinButton,INPUT_PULLUP);
-  pinMode(pwm1, OUTPUT);
-  pinMode(pwm2, OUTPUT);
-  Serial.begin(9600);
-  nfc.begin();
-  nfc.SAMConfig();
-//  lcd.setCursor(3, 1);
-//  lcd.print("TAP KARTU ANDA");
+  lcd.clear();
+//  pinMode(relayAtas1, OUTPUT);
+//  pinMode(relayAtas2, OUTPUT);
+//  pinMode(relayAtas3, OUTPUT);
+//  pinMode(relayAtas4, OUTPUT);
+//  pinMode(relayAtas5, OUTPUT);
+//  pinMode(relayBawah1, OUTPUT);
+//  pinMode(relayBawah2, OUTPUT);
+//  pinMode(relayBawah3, OUTPUT);
+//  pinMode(relayBawah4, OUTPUT);
+//  pinMode(relayBawah5, OUTPUT);
+  for(int x=0;x<=4;x++) //aktivator pin untuk seluruh relay
+  {
+//    pinMode(relayAtas[x], OUTPUT);
+    pinMode(relayBawah[x], OUTPUT);
+  }
+  Serial.println("ready");
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("ready");
 }
-
-void loop()
+ 
+void loop() 
 {
-  lcd.setCursor(3, 1);
-  lcd.print("TAP KARTU ANDA");
-  getNfcIdUser();
-//  while(1)
-//  {
-//    Serial.println(digitalRead(pinButton));
-//  }
-//  if(digitalRead(pinButton)==LOW)
-//  {
-//   analogWrite(pwm1, 0);
-//   analogWrite(pwm2, 200);
-//   delay(700);
-//   analogWrite(pwm1, 0);
-//   analogWrite(pwm2, 0);
-//   delay(1000);
-//   analogWrite(pwm1, 200);
-//   analogWrite(pwm2, 0);
-//   delay(500); 
-//  }
-//  else
-//  {
-//   analogWrite(pwm1, 0);
-//   analogWrite(pwm2, 0); 
-//  }
-  
+  if(Serial.available()>0)
+  {
+    char inChar = (char)Serial.read();
+    dataIn += inChar;
+    if (inChar == '\n') {parsing = true;} // jika parsing oke 
+  }
+  if(parsing)
+  {
+    parsingData();
+    prosesBuka();
+    parsing=false;
+    dataIn="";      
+  }
+}
+void parsingData()
+{
+  int j=0;
+  //kirim data yang telah diterima sebelumnya
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("in:");
+//  lcd.print(1,2);
+  lcd.print(dataIn);
+  delay(500);
+//  Serial.print("\n");
+  //inisialisasi variabel, (reset isi variabel)
+  dataBukaShaf[j]="";
+  //proses parsing data
+  for(i=0;i<dataIn.length();i++)
+  {
+   //pengecekan tiap karakter dengan karakter (#) dan (,)
+   if (dataIn[i] == ',')
+   {
+     //increment variabel j, digunakan untuk merubah index array penampung
+     j++;
+     dataBukaShaf[j]=""; //inisialisasi variabel array dt[j]
+   }
+   else
+   {
+     //proses tampung data saat pengecekan karakter selesai.
+     dataBukaShaf[j] = dataBukaShaf[j] + dataIn[i];
+   }
+  }
+      //kirim data hasil parsing
+//    Serial.print("Shaf 1 : ");
+//    Serial.print(dataBukaShaf[0].toInt());
+//    Serial.print("\n");
+//    Serial.print("Shaf 2 : ");
+//    Serial.print(dataBukaShaf[1].toInt());
+//    Serial.print("\n");
+//    Serial.print("Shaf 3 : ");
+//    Serial.print(dataBukaShaf[2].toInt());
+//    Serial.print("\n");
+//    Serial.print("Shaf 4 : ");
+//    Serial.print(dataBukaShaf[3].toInt());
+//    Serial.print("\n");
+//    Serial.print("Shaf 5 : ");
+//    Serial.print(dataBukaShaf[4].toInt());
+//    Serial.print("\n\n");
+}
+void resetData()
+{
+  for(int x=0;x<=4;x++)
+  {
+    dataBukaShaf[x]="0";
+  }
+}
+void prosesBuka()
+{
+  for(int jumlahShaf=0;jumlahShaf<=4;jumlahShaf++)
+  {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Shaf ke-");
+    lcd.print(jumlahShaf + 1); // di plus 1 hanya untuk lihat di print aja
+//    Serial.println();
+    for(int jumlahOpen=1;jumlahOpen<=dataBukaShaf[jumlahShaf].toInt();jumlahOpen++)
+    {
+     lcd.print("$");
+     digitalWrite(relayBawah[jumlahShaf], HIGH);
+     delay(500);
+     digitalWrite(relayBawah[jumlahShaf], LOW);
+     delay(500);
+    }
+//     Serial.println();
+  }
+  resetData();
+  setup();
 }
